6c3ebda9aba1d0c5f58a5946a28ff0d7995e6990
rumori <intonarumori@gmail.com>
2016-03-24 14:25:23 +0100

    Initial Commit

---
+++ UIViewNibExtensionsExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIViewNibExtensionsExample
+//
+//  Created by Daniel Langh on 24/03/16.
+//  Copyright © 2016 rumori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIViewNibExtensionsExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIViewNibExtensionsExample
+//
+//  Created by Daniel Langh on 24/03/16.
+//  Copyright © 2016 rumori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
