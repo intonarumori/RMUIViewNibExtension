3c508a16340480b267b29988cd0fb3c510c95ce4
rumori <intonarumori@gmail.com>
2016-04-09 15:40:20 +0200

    updated appdelegate to swift

---
+++ UIViewNibExtensionsExample/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  UIViewNibExtensionsExample
+//
+//  Created by Daniel Langh on 09/04/16.
+//  Copyright © 2016 rumori. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window:UIWindow?
+}
